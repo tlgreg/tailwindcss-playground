fb9490cf538378a5b1de112e4baa5609857e96fd
TLGreg <666791+tlgreg@users.noreply.github.com>
2020-04-12 19:04:16 +0200

    added extra postcss plugins to config, replaced autoprefixer with preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,17 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./public/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
-    require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
+    require('postcss-nested'),
+    require('postcss-font-magician'),
+    require('postcss-preset-env'),
+    ...(process.env.NODE_ENV === 'production'
       ? [purgecss, require('cssnano')]
-      : []
-  ]
+      : []),
+  ],
 }
